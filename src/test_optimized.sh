0e0002da2a5bd76de390157978afde884b2359ed
Andrey Nechesov <alnechesov@edu.hse.ru>
2022-10-23 22:42:50 +0300

    renew

--- src/test_optimized.sh
+++ src/test_optimized.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 gcc optimized.s -o optimized
-../test.py --n 100 ./optimized > log
-grep -c log -e "\bCorrect"
+../test.py --n 100 ./optimized > ../logs/log_optimized
+grep -c ../logs/log_optimized -e "\bCorrect"
